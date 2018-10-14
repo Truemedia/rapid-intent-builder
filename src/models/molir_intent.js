1f059331589821d82eda7ec5dc76dfdae3c02710
Wade Penistone <wadespackages@yahoo.com>
2018-10-14 12:41:40 +0100

    Fixed typo

--- src/models/molir_intent.js
+++ src/models/molir_intent.js
@@ -5,7 +5,7 @@ module.exports = class MolirIntent extends Intent {
     {
         return {
             intentName: this.name,
-            uttterances: this.samples,
+            utterances: this.samples,
             keywords: this.slots
         };
     }
