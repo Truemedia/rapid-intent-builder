91d154f9261a8e9da077a5ba937e082573282331
Wade Penistone <wadespackages@yahoo.com>
2019-04-18 17:39:32 +0100

    Rasa adapter functional

--- src/adapters/rasa/index.js
+++ src/adapters/rasa/index.js
@@ -1,10 +1,22 @@
 const Adapter = require('./../adapter');
+const Intent = require('./intent');
+const Intents = require('./intents');
 
 class RasaAdapter extends Adapter
 {
   constructor()
   {
     super();
+  }
+
+  get intent()
+  {
+    return Intent;
+  }
+
+  get intents()
+  {
+    return Intents;
   }
 }
 
--- src/adapters/rasa/intents.js
+++ src/adapters/rasa/intents.js
@@ -11,7 +11,9 @@ class RasaIntents extends Intents
   {
     return {
       "rasa_nlu_data": {
-        "common_examples": ...this.intents,
+        "common_examples": [].concat(...this.intents.map(
+          intent => intent.toJson()
+        )),
         "regex_features" : [],
         "lookup_tables"  : [],
         "entity_synonyms": []
