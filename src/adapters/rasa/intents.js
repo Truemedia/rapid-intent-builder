13b880b35b5d3ef5712b0e076e9e0c9ea6761725
Wade Penistone <wadespackages@yahoo.com>
2019-04-18 19:30:14 +0100

    Rasa cleanup and filename

--- src/adapters/rasa/intents.js
+++ src/adapters/rasa/intents.js
@@ -9,15 +9,48 @@ class RasaIntents extends Intents
 
   toJson()
   {
+    let {rasa_nlu_data} = this;
+    return {rasa_nlu_data};
+  }
+
+  get rasa_nlu_data()
+  {
+    let {common_examples, regex_features, lookup_tables, entity_synonyms} = this;
+    return {common_examples, regex_features, lookup_tables, entity_synonyms};
+  }
+
+  get common_examples()
+  {
+    return [].concat(...this.intents.map(
+      intent => intent.toJson()
+    ));
+  }
+
+  get regex_features()
+  {
+    return []; // TODO: Flesh out
+  }
+
+  get lookup_tables()
+  {
+    return []; // TODO: Flesh out
+  }
+
+  get entity_synonyms()
+  {
+    return []; // TODO: Flesh out
+  }
+
+  get dest() // TODO: Make contributing options configurable
+  {
+    let filename = this.filename();
+    return `models/current/nlu/${filename}`;
+  }
+
+  get filenames()
+  {
     return {
-      "rasa_nlu_data": {
-        "common_examples": [].concat(...this.intents.map(
-          intent => intent.toJson()
-        )),
-        "regex_features" : [],
-        "lookup_tables"  : [],
-        "entity_synonyms": []
-      }
+      json: 'training_data'
     };
   }
 }
